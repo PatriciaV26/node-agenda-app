d7bb49b7f0abfc9d50d597f14bdf76fdfe442452
Patricia Veres <veres_patricia@yahoo.com>
2019-04-16 11:53:33 +0300

    send data to users/add

--- public/javascripts/agenda.js
+++ public/javascripts/agenda.js
@@ -1,5 +1,14 @@
 var allPersons = [];
 
+var API_URL = {
+    //ADD: 'data/add.json'
+    ADD: 'users/add'
+};
+var API_METHOD = {
+    //ADD: 'GET'
+    ADD: 'POST'
+}
+
 fetch('data/persons.json').then(function (r) {
     return r.json();
 }).then(function (persons) {
@@ -12,39 +21,46 @@ function display(persons) {
         return `<tr><td>${a.firstName}</td><td>${a.lastName}</td><td>${a.phone}</td><td></td></tr>`;
     });
     document.querySelector('#agenda tbody').innerHTML = list.join("");
-    
+
 }
 
-function savePerson(){
+function savePerson() {
     var firstName = document.querySelector('[name=firstName]').value; //getting the value out of the input
-    var lastName = document.querySelector('[name=lastName]').value; 
-    var phone = document.querySelector('[name=phone]').value; 
-    
+    var lastName = document.querySelector('[name=lastName]').value;
+    var phone = document.querySelector('[name=phone]').value;
+
     submitNewPerson(firstName, lastName, phone);
 }
 
 function submitNewPerson(firstName, lastName, phone) {
-    
+
     var body = null;
-    fetch('data/add.json', {
-        method: 'GET', //can't send info directly with get
-        body: body
-        //body: JSON.stringify({
-            //firstName: firstName,
-            //lastName: lastName,
-            //phone: phone
-        //})
-    }).then(function(r){
+    if (API_METHOD.ADD === 'POST') {
+        //fetch(API_URL.ADD, {
+        //method: API_METHOD.ADD,can't send info directly with get
+        body = JSON.stringify({
+            firstName: firstName,
+            lastName: lastName,
+            phone: phone
+        });
+    }
+    fetch(API_URL.ADD, {
+        method: API_METHOD.ADD,
+        body: body,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then(function (r) {
         return r.json();
-    }).then(function(status){
-        if(status.success){
+    }).then(function (status) {
+        if (status.success) {
             inlineAddPerson(firstName, lastName, phone);
         } else {
             console.warn('not saved', status);
         }
-    });
+    })
 }
-
+    
 function inlineAddPerson(firstName, lastName, phone) {
     allPersons.push({
         firstName: firstName,
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/add', function(req, res, next) {
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var phone = req.body.phone;
+  console.warn('add', firstName, lastName, phone);
+  var persons = require('../public/data/persons.json');
+  persons.push({
+    firstName,
+    lastName,
+    phone
+  });
+  var str = JSON.stringify(persons, null, 2);
+  fs.writeFileSync('./public/data/persons.json', str);
+  //to do: save this data in persons.json
+  res.json({
+    succes: true,
+    message: 'to do'
+  });
 });
 
 module.exports = router;
